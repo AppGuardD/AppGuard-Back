94a5dace927c434c65d79ec9cf825ec3998ec224
JlopezArrieta <javilop87@hotmail.com>
2024-01-24 11:19:07 -0500

    creacion de carpetas routes

--- back-end/src/routes/routes.ts
+++ back-end/src/routes/routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
 import mangrulloRoutes from "./mangrulloRoutes/mangrulloRoutes";
 import activityRouter from "./activityRoutes/activityRoutes";
+import adviceRoutes from "./adviceRoutes/adviceRoutes";
+import favoritelloRoutes from "./favoritelloRoutes/favoritelloRoutes";
+import userRoutes from "./favoritelloRoutes/favoritelloRoutes";
+import reviewActivityRoutes from "./reviewActivityRoutes/reviewActivityRoutes";
+import reviewMangrullosRoutes from "./reviewMangrullosRoutes/reviewMangrullosRoutes";
+
 
 const routes = Router();
 
 routes.use("/activities", activityRouter);
 routes.use("/mangrullos", mangrulloRoutes);
+routes.use("/advice", adviceRoutes);
+routes.use("/favorite", favoritelloRoutes);
+routes.use("/user", userRoutes);
+routes.use("/reviewActivity", reviewActivityRoutes);
+routes.use("/reviewMangrullos", reviewMangrullosRoutes);
+//routes.use("/ticket", ticketRoutes);
+
+
 
 export default routes;
