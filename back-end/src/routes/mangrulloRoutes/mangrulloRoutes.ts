038e01e332c7b0e5a32b9a9b84e45755d54168f2
Nestor Camilo <nlondono609@gmail.com>
2024-02-05 12:32:02 -0500

    se comentaron los adminMidleware

--- back-end/src/routes/mangrulloRoutes/mangrulloRoutes.ts
+++ back-end/src/routes/mangrulloRoutes/mangrulloRoutes.ts
@@ -9,18 +9,18 @@ import { upload } from "../../helper/multer/multerConfig";
 
 const mangrulloRoutes = Router();
 //-----------con webtokens-------------
-mangrulloRoutes.get("/search", getMangrullos);
+/* mangrulloRoutes.get("/search", getMangrullos);
 mangrulloRoutes.get("/search/:id", getIdMangrullo);
 mangrulloRoutes.post("/create", adminMiddleware, upload.single("image"), postMangrullos);
 mangrulloRoutes.put("/update/:id", adminMiddleware, upload.single("image"), putMangrullo);
-mangrulloRoutes.put("/deactivate/:id", adminMiddleware, disableMangrullo);
+mangrulloRoutes.put("/deactivate/:id", adminMiddleware, disableMangrullo); */
 //----------Desarollo------------------
 
-// mangrulloRoutes.get("/name/:name", nameMangrullo);
-// mangrulloRoutes.get("/search", getMangrullos);
-// mangrulloRoutes.get("/search/:id", getIdMangrullo);
-// mangrulloRoutes.post("/create", upload.single("image"), postMangrullos);
-// mangrulloRoutes.put("/update/:id", upload.single("image"), putMangrullo);
-// mangrulloRoutes.put("/disable/:id", disableMangrullo);
+//mangrulloRoutes.get("/name/:name", nameMangrullo);
+mangrulloRoutes.get("/search", getMangrullos);
+mangrulloRoutes.get("/search/:id", getIdMangrullo);
+mangrulloRoutes.post("/create", upload.single("image"), postMangrullos);
+mangrulloRoutes.put("/update/:id", upload.single("image"), putMangrullo);
+mangrulloRoutes.put("/disable/:id", disableMangrullo);
 
 export default mangrulloRoutes;
