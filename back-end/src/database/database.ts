1b9021a4cd82b612b4acf8e82e38081ddd492390
Nestor Camilo <nlondono609@gmail.com>
2024-02-05 09:57:06 -0500

    se arreglo la conexion a la base de datos remota

--- back-end/src/database/database.ts
+++ back-end/src/database/database.ts
@@ -15,34 +15,31 @@ import { Favorite } from "../models/favorite/favorite";
 import { FavoriteMangrullo } from "../models/favorite/FavoriteMangrullo";
 import { TicketActivity } from "../models/ticket/TicketActivity";
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, EXTERNAL_DB_URL }: any =
-  process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, EXTERNAL_DB_URL }: any = process.env;
 
-export const connection = new Sequelize(
-  /* EXTERNAL_DB_URL,*/ {
-    dialect: "postgres",
-    host: DB_HOST,
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    // dialectOptions: { ssl: { require: true } },
-    logging: false, //console.log,
-    models: [
-      Activity,
-      Advice,
-      Favorite,
-      Mangrullo,
-      ReviewActivity,
-      ReviewMangrullo,
-      ActivityMangrullo,
-      FavoriteMangrullo,
-      User,
-      Session,
-      Ticket,
-      TicketActivity
-    ],
-  },
-);
+export const connection = new Sequelize(EXTERNAL_DB_URL, {
+  //dialect: "postgres",
+  //host: DB_HOST,
+  //username: DB_USER,
+  //password: DB_PASSWORD,
+  //database: DB_DATABASE,
+  // dialectOptions: { ssl: { require: true } },
+  logging: false, //console.log,
+  models: [
+    Activity,
+    Advice,
+    Favorite,
+    Mangrullo,
+    ReviewActivity,
+    ReviewMangrullo,
+    ActivityMangrullo,
+    FavoriteMangrullo,
+    User,
+    Session,
+    Ticket,
+    TicketActivity,
+  ],
+});
 
 async function connectionDB() {
   try {
