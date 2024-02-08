ed307579d2904545ec99c0b6744e0517e664ebe6
Nestor Camilo <nlondono609@gmail.com>
2024-02-08 10:06:12 -0500

    se termino la facturacion de mercadoPago

--- back-end/src/controllers/mercadoPago/getSuccess/getSuccess.ts
+++ back-end/src/controllers/mercadoPago/getSuccess/getSuccess.ts
@@ -1,11 +1,35 @@
 import { Request, Response } from "express";
-import { paymentActivities } from "../../../services/mercadoPagoServices/mercadoPago";
+import {
+  merchantOders,
+  paymentActivities,
+  requirePayInfo,
+} from "../../../services/mercadoPagoServices/mercadopagoConfig/mercadoPago";
+import { facture } from "../../../services/mercadoPagoServices/mercadoPagoFactureEmail/Facture";
+import { sendMail } from "../../../helper/nodeMail/nodeMail";
+import { User } from "../../../models/user/user";
+import { SentMessageInfo } from "nodemailer";
 
-export const getSucces = async (req: Request, res: Response) => {
+export const getSucces = async (
+  req: Request<{}, {}, {}, { payment_id: string }>,
+  res: Response
+) => {
   try {
     const info = req.query;
-    console.log(info);
-    res.status(201).send({ ...info });
+    const paymentInfo = await requirePayInfo(info?.payment_id);
+    const userId = paymentInfo.external_reference.split(",")[1];
+    const user = await User.findOne({ where: { id: userId } });
+    const htmlForEmail = facture(paymentInfo.additional_info.items);
+    const emailFacture: SentMessageInfo = await sendMail(
+      "Factura de compra en AppGuard",
+      `${user?.email}`,
+      "aqui te dejamos tu factura",
+      htmlForEmail
+    );
+    if (emailFacture?.accepted?.length <= 0) {
+      console.log("no se ha podido enviar el objeto");
+    }
+
+    res.send(user);
   } catch (error: any) {
     res.status(400).send({ error: error.message });
   }
