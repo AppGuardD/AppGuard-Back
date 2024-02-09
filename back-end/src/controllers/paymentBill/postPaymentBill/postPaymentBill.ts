1ab9dc592b99147ec639229c9681b2670440a8e4
Nestor Camilo <nlondono609@gmail.com>
2024-02-08 23:56:10 -0500

    se resolvieron bugs

--- back-end/src/controllers/paymentBill/postPaymentBill/postPaymentBill.ts
+++ back-end/src/controllers/paymentBill/postPaymentBill/postPaymentBill.ts
@@ -8,48 +8,46 @@ export const postPaymentBill: RequestHandler = async (req, res) => {
   try {
     const { totalValue, userId, carId } = req.body;
 
-        //Verificar que los campos no estén vacíos.
-        if (!totalValue || !userId || !carId) {
-            return res.status(302).json({ message: "Todos los campos son requeridos" });
-        }
-
-        const idUser: User | null = await User.findByPk(userId);
-        if (!idUser) {
-            return res
-                .status(302)
-                .json({ message: "El usuario no existe en la base de datos" });
-        }
-
-        // const idCar: Car | null = await Car.findByPk(carId);
-        // if (!idCar) {
-        //     return res
-        //         .status(302)
-        //         .json({ message: "El carrito no existe en la base de datos" });
-        // }
-
-
-        //PaymentBill esta definido como un objeto de PaymentBill.
-        const paymentBill: PaymentBill | null = await PaymentBill.create({
-            date: new Date().toISOString(),
-            totalValue: totalValue,
-            //state: state,
-            userId: userId,
-            carId: carId
-        });
-        return res
-            .status(201)
-            .json({
-                message: "PaymentBill realizada con exito", PaymentBill: paymentBill
-            });
-    } catch (error: any) {
-        return res
-            .status(500)
-            .json({
-                message: "Algo salió mal, verifica la función", error: error.message,
-            });
+    //Verificar que los campos no estén vacíos.
+    if (!totalValue || !userId || !carId) {
+      return res.status(302).json({ message: "Todos los campos son requeridos" });
     }
 
     const idUser: User | null = await User.findByPk(userId);
+    if (!idUser) {
+      return res
+        .status(302)
+        .json({ message: "El usuario no existe en la base de datos" });
+    }
+
+    // const idCar: Car | null = await Car.findByPk(carId);
+    // if (!idCar) {
+    //     return res
+    //         .status(302)
+    //         .json({ message: "El carrito no existe en la base de datos" });
+    // }
+
+    //PaymentBill esta definido como un objeto de PaymentBill.
+    const paymentBill: PaymentBill | null = await PaymentBill.create({
+      date: new Date().toISOString(),
+      totalValue: totalValue,
+      //state: state,
+      userId: userId,
+      carId: carId,
+    });
+    return res.status(201).json({
+      message: "PaymentBill realizada con exito",
+      PaymentBill: paymentBill,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: "Algo salió mal, verifica la función",
+      error: error.message,
+    });
+  }
+};
+
+/*     const idUser: User | null = await User.findByPk(userId);
     if (!idUser) {
       return res
         .status(302)
@@ -82,3 +80,4 @@ export const postPaymentBill: RequestHandler = async (req, res) => {
     });
   }
 };
+ */
