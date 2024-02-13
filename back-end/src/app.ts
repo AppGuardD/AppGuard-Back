7635f02b545ac7e236b6bc9baa517f47d5e7635a
JlopezArrieta <javilop87@hotmail.com>
2024-02-13 15:13:12 -0500

    configurcion google

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -41,6 +41,18 @@ app.use(passport.session());
 app.use(morgan("dev"));
 app.use(cors());
 
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
